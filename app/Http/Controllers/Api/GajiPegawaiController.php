a812f6d0fa985c8bb0ec9b61572886bc11b41842
rsvn.id <emailrizkyprasetyo@gmail.com>
2022-01-08 13:40:17 +0700

    initial commit

--- app/Http/Controllers/Api/GajiPegawaiController.php
+++ app/Http/Controllers/Api/GajiPegawaiController.php
@@ -4,11 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ProgramResourceGajiPegawai;
-use App\Http\Resources\ProgramResourceGajiPegawaiView;
 use App\Models\GajiPegawai;
 use App\Models\Pegawai;
 use Illuminate\Http\Request;
-use Validator;
 
 class GajiPegawaiController extends Controller
 {
@@ -19,7 +17,20 @@ class GajiPegawaiController extends Controller
      */
     public function index()
     {
-        $data = GajiPegawai::with('pegawai')->latest()->paginate(2);
+        $columns = [
+            'id_pegawai',
+            'tota_gaji_diterima',
+            'created_at'
+        ];
+        $data = GajiPegawai::with('pegawai')->select($columns)->latest()->paginate(2);
+        foreach ($data as $index => $rows) {
+            $data[$index]['tota_gaji_diterima']       = number_format($rows['tota_gaji_diterima'], 2, ".", ",");
+
+            foreach ($rows as $key => $value) {
+                if (array_key_exists($key, $columns) && !is_null($value))
+                    $data[$index][$key] = $columns[$key][$value];
+            }
+        }
         return response()->json($data);
     }
 
@@ -30,23 +41,18 @@ class GajiPegawaiController extends Controller
      */
     public function store(Request $request)
     {
-        $cekIdPegawai = Pegawai::where('nama_pegawai',$request->nama_pegawai)->first();
+        $cekIdPegawai = Pegawai::where('nama_pegawai', $request->nama_pegawai)->first();
         if (!empty($cekIdPegawai->id)) {
             $dpegawai = GajiPegawai::updateOrCreate([
                 'id_pegawai' => $cekIdPegawai->id,
                 'tota_gaji_diterima' => $cekIdPegawai->total_gaji,
-             ]);
+            ]);
             return response()->json(['Gaji Pegawai created successfully.', new ProgramResourceGajiPegawai($dpegawai)]);
-        }else{
+        } else {
             return response()->json([
                 'status'    => 400,
                 'message'   => 'Pegawai Tidak Ada'
-            ]);       
-
+            ]);
         }
-        
-
-
     }
-
 }
--- app/Http/Controllers/Api/PegawaiControlller.php
+++ app/Http/Controllers/Api/PegawaiControlller.php
@@ -29,11 +29,11 @@ class PegawaiControlller extends Controller
         $data = Pegawai::orderBy('created_at', 'desc')->select($columns)->paginate(2);
         foreach ($data as $index => $rows) {
             $data[$index]['nama_pegawai']             = strtoupper(substr($rows['nama_pegawai'], 0, 6));
-            $data[$index]['total_gaji']               = number_format($rows['total_gaji'],2,".",",");
+            $data[$index]['total_gaji']               = number_format($rows['total_gaji'], 2, ".", ",");
             $data[$index]['created_at']               = Carbon::parse($rows['created_at'])->format('D/M/Y');
-            
+
             foreach ($rows as $key => $value) {
-                if (array_key_exists($key, $columns) && ! is_null($value))
+                if (array_key_exists($key, $columns) && !is_null($value))
                     $data[$index][$key] = $columns[$key][$value];
             }
         }
@@ -47,22 +47,32 @@ class PegawaiControlller extends Controller
      */
     public function store(Request $request)
     {
-        $validator = Validator::make($request->all(),[
+        $validator = Validator::make($request->all(), [
             'nama_pegawai' => 'required|string|max:10|unique:pegawai,nama_pegawai',
             'total_gaji'   => 'required|integer|',
         ]);
 
-        if($validator->fails()){
-            return response()->json($validator->errors());       
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
         }
 
+        if ($request->total_gaji <= '4000000') {
+            return response()->json([
+                'status'    => 400,
+                'message'   => 'Total Gaji Minimal 4000000 '
+            ]);
+        } elseif ($request->total_gaji >= '10000000') {
+            return response()->json([
+                'status'    => 400,
+                'message'   => 'Total Gaji Maksimal 10000000 '
+            ]);
+        }
+        
         $dpegawai = Pegawai::updateOrCreate([
             'nama_pegawai' => $request->nama_pegawai,
             'total_gaji'   => $request->total_gaji,
-         ]);
+        ]);
 
-         return response()->json(['Pegawai created successfully.', new ProgramResource($dpegawai)]);
-
-
+        return response()->json(['Pegawai created successfully.', new ProgramResource($dpegawai)]);
     }
 }
